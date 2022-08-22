4534b03b54f0a0fcbacb9772831ba24475a4cdee
Satti babu <sattibabuchittala@gmail.com>
2022-08-22 12:48:16 +0530

    update

--- src/main/java/com/careeco/studentmanagement/model/Department.java
+++ src/main/java/com/careeco/studentmanagement/model/Department.java
@@ -4,10 +4,9 @@ package com.careeco.studentmanagement.model;//sample file i have created
 public class Department{
 
     private  String deptId;
-    private String departmentName;
+
     private String collegeName;
-    private String hodName;
-    private String deptNumber;
+    private String name;
 
 
 }
