9e6c9b4b41fdea684a7a3fe162172e5e82d93f67
Satti babu <sattibabuchittala@gmail.com>
2022-08-25 03:46:46 -0700

    IVM-13 | @Schiita | added getters & Setters for Department Model

--- src/main/java/com/careeco/studentmanagement/model/Department.java
+++ src/main/java/com/careeco/studentmanagement/model/Department.java
@@ -7,4 +7,37 @@ public class Department{
     private String departmentName;
     private String hodOfDept;
 
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getHodOfDept() {
+        return hodOfDept;
+    }
+
+    public void setHodOfDept(String hodOfDept) {
+        this.hodOfDept = hodOfDept;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "deptId='" + deptId + '\'' +
+                ", departmentName='" + departmentName + '\'' +
+                ", hodOfDept='" + hodOfDept + '\'' +
+                '}';
+    }
 }
